dfa43f38091bbcd712caa7984f1ace1c7aa25615
Jabulani Mpofu <jabulani.saisai@gmail.com>
2020-10-21 21:40:56 +0200

    My first action is ready1

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function main(): Promise<void> {
     //start
     // `who-to-greet` input defined in action metadata file
     const nameToGreet = core.getInput('who-to-greet')
-    console.log(`Hello iwe ndiwe ani ${nameToGreet}!`)
+    console.log(`Hello iwe ndiwe ani ko ${nameToGreet}!`)
     const time = new Date().toTimeString()
     core.setOutput('time', time)
     // Get the JSON webhook payload for the event that triggered the workflow
