8e6c8a3dd60e9068f68731ea2ae86cbe9263fcba
Jabulani Mpofu <jabulani.saisai@gmail.com>
2020-10-21 20:53:39 +0200

    getting some data

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,27 @@
-import core from '@actions/core';
-import github from '@actions/github';
+import core from '@actions/core'
+import github from '@actions/github'
+import {execSync} from 'child_process'
+import fs from 'fs'
 
-try {
+async function main(): Promise<void> {
+  try {
+    const repoName = github.context.repo.repo
+    console.log('repoName: ', repoName);
+    const repoOwner = github.context.repo.owner
+    console.log('repoOwner: ', repoOwner);
+    const githubToken = core.getInput('accessToken')
+    console.log('githubToken: ', githubToken);
+    // const fullCoverage = JSON.parse(core.getInput('fullCoverageDiff'))
+    // const commandToRun = core.getInput('runCommand')
+    const githubClient = github.getOctokit(githubToken)
+    console.log('githubClient: ', githubClient);
+    const prNumber = github.context.issue.number
+    console.log('prNumber : ', prNumber );
+    const branchNameBase = github.context.payload.pull_request?.base.ref
+    console.log('branchNameBase: ', branchNameBase);
+    const branchNameHead = github.context.payload.pull_request?.head.ref
+    console.log('branchNameHead: ', branchNameHead);
+    //start
     // `who-to-greet` input defined in action metadata file
     const nameToGreet = core.getInput('who-to-greet')
     console.log(`Hello ${nameToGreet}!`)
@@ -10,6 +30,8 @@ try {
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
     console.log(`The event payload: ${payload}`)
-} catch (error) {
+  } catch (error) {
     core.setFailed(error.message)
+  }
 }
+main()
