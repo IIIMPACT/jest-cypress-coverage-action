5ec03c0fe411f38b556596b75d44efa0f5787d4c
Jabulani Mpofu <jabulani.saisai@gmail.com>
2020-10-21 22:51:05 +0200

    some stuff

--- src/main.ts
+++ src/main.ts
@@ -3,22 +3,30 @@
 /* eslint-disable no-console */
 const github = require('@actions/github')
 const core = require('@actions/core')
-// import {execSync} from 'child_process'
+import {execSync} from 'child_process'
 // import fs from 'fs'
 
 async function main(): Promise<void> {
   try {
-    //start
-    // `who-to-greet` input defined in action metadata file
-    console.log('core: ', core)
-    console.log('github: ', github)
-    const nameToGreet = core.getInput('who-to-greet')
-    console.log(`Hello iwe ndiwe ani ko iwe nhai ${nameToGreet}!`)
-    const time = new Date().toTimeString()
-    core.setOutput('time', time)
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload yachinja here: ${payload}`)
+    const repoName = github.context.repo.repo
+    console.log('repoName: ', repoName)
+    const repoOwner = github.context.repo.owner
+    console.log('repoOwner : ', repoOwner)
+    // const githubToken = core.getInput('accessToken')
+    // const fullCoverage = JSON.parse(core.getInput('fullCoverageDiff'))
+    // const commandToRun = core.getInput('runCommand')
+    // const githubClient = github.getOctokit(githubToken)
+    // console.log('githubClient: ', githubClient);
+    const prNumber = github.context.issue.number
+    console.log('prNumber: ', prNumber)
+    const branchNameBase = github.context.payload.pull_request?.base.ref
+    console.log('branchNameBase: ', branchNameBase)
+    const branchNameHead = github.context.payload.pull_request?.head.ref
+    console.log('branchNameHead: ', branchNameHead)
+    execSync('echo "it can run stuff"')
+    // const codeCoverageNew = <CoverageReport>(
+    //   JSON.parse(fs.readFileSync('coverage-summary.json').toString())
+    // )
   } catch (error) {
     core.setFailed(error.message)
   }
