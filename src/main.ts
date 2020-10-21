0b87031bd66f54bb0e266f32ae439dd75dded599
Jabulani Mpofu <jabulani.saisai@gmail.com>
2020-10-21 21:39:51 +0200

    My first action is ready

--- src/main.ts
+++ src/main.ts
@@ -6,22 +6,6 @@ import github from '@actions/github'
 
 async function main(): Promise<void> {
   try {
-    const repoName = github.context.repo.repo
-    console.log('repoName: ', repoName)
-    const repoOwner = github.context.repo.owner
-    console.log('repoOwner: ', repoOwner)
-    const githubToken = core.getInput('accessToken')
-    console.log('githubToken: ', githubToken)
-    // const fullCoverage = JSON.parse(core.getInput('fullCoverageDiff'))
-    // const commandToRun = core.getInput('runCommand')
-    const githubClient = github.getOctokit(githubToken)
-    console.log('githubClient: ', githubClient)
-    const prNumber = github.context.issue.number
-    console.log('prNumber : ', prNumber)
-    const branchNameBase = github.context.payload.pull_request?.base.ref
-    console.log('branchNameBase: ', branchNameBase)
-    const branchNameHead = github.context.payload.pull_request?.head.ref
-    console.log('branchNameHead: ', branchNameHead)
     //start
     // `who-to-greet` input defined in action metadata file
     const nameToGreet = core.getInput('who-to-greet')
