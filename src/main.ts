37efd84b8b1f8063f48c183d935e1adae46ddb56
Jabulani Mpofu <jabulani.saisai@gmail.com>
2020-10-26 12:32:48 +0200

    added li

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 // /* eslint-disable @typescript-eslint/camelcase */
-// /* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable @typescript-eslint/no-require-imports */
 /* eslint-disable no-console */
@@ -8,9 +8,12 @@ const core = require('@actions/core')
 const execSync = require('child_process').execSync
 // const fs = require('fs-extra')
 const fs = require('fs')
-// const fs1 = require('fs-extra')
+const fs1 = require('fs-extra')
 
-// const {exec} = require('child_process')
+const {createCoverageMap} = require('istanbul-lib-coverage')
+const {createReporter} = require('istanbul-api')
+
+const {exec} = require('child_process')
 
 // const DiffChecker = require('./DiffChecker').DiffChecker
 // import fs from 'fs'
@@ -22,14 +25,76 @@ const fs = require('fs')
 //   return pullRequestId
 // }
 
+type FileNamesMap = {[key: string]: boolean} | null
+
+const result = async (command: string): Promise<string | Buffer> => {
+  return new Promise((resolve, reject) => {
+    exec(
+      command,
+      (err: Error, stdout: string | Buffer, stderr: string | Buffer) => {
+        if (err !== null) {
+          reject(err)
+          return
+        }
+        if (typeof stderr !== 'string') {
+          reject(stderr)
+          return
+        }
+        resolve(stdout)
+      }
+    )
+  })
+}
+
 async function mergeJestCypressCoverage(
   reportFiles: string[],
   reporters: string[] = ['json', 'html'],
   baseRef?: string
 ): Promise<void> {
   console.log('changedSince', baseRef)
-  console.log('changedSince', reporters)
-  console.log('changedSince', baseRef)
+
+  const map = createCoverageMap({})
+
+  let fileNamesMap: FileNamesMap = {}
+
+  if (baseRef) {
+    const headRef =
+      process.env.GITHUB_HEAD_REF || (await result('git branch --show-current'))
+
+    const fileNamesStr = await result(
+      `git diff --name-only ${baseRef} origin/${headRef}`
+    )
+
+    fileNamesMap = (fileNamesStr as string).split('\n').reduce(
+      (acc, fileName) => ({
+        ...acc,
+        [`${process.cwd()}/${fileName}`]: true
+      }),
+      {}
+    )
+  }
+
+  for (const file of reportFiles) {
+    const r = fs1.readJsonSync(file)
+    const o = Object.entries<{data: any}>(r).reduce(
+      (acc: {[key: string]: any}, [key, value]) => {
+        let output = value
+        if (value.data) {
+          output = value.data
+        }
+        if (!fileNamesMap || fileNamesMap[key]) {
+          acc[key] = output
+        }
+        return acc
+      },
+      {}
+    )
+    map.merge(o)
+  }
+
+  const reporter = createReporter()
+  reporter.addAll(reporters)
+  reporter.write(map)
 }
 
 async function main(): Promise<void> {
